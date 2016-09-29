5e233dfa886e4d708c8f31c2d55d7f71681994ad
Jaimyoung Kwon <statkwon@gmail.com>
2016-09-29 15:40:00 -0700

    Start glm

---
+++ ch08-classification/breast-cancer/breast-cancer.R
@@ -0,0 +1,217 @@
+# 빅데이터 분별분석. 암 예측.
+#
+if (!file.exists("breast-cancer-wisconsin.data")){
+  system('curl http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data > breast-cancer-wisconsin.data')
+  system('curl http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.names > breast-cancer-wisconsin.names')
+}
+
+binomial_deviance <- function(y_obs, yhat){
+  epsilon = 0.0001
+  yhat = ifelse(yhat < epsilon, epsilon, yhat)
+  yhat = ifelse(yhat > 1-epsilon, 1-epsilon, yhat)
+  a = ifelse(y_obs==0, 0, y_obs * log(y_obs/yhat))
+  b = ifelse(y_obs==1, 0, (1-y_obs) * log((1-y_obs)/(1-yhat)))
+  return(2*sum(a + b))
+}
+
+
+panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...){
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y))
+  txt <- format(c(r, 0.123456789), digits = digits)[1]
+  txt <- paste0(prefix, txt)
+  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
+  text(0.5, 0.5, txt, cex = cex.cor * r)
+}
+
+
+
+library(dplyr)
+library(ggplot2)
+library(MASS)
+library(glmnet)
+library(randomForest)
+library(gbm)
+library(rpart)
+library(boot)
+library(data.table)
+library(ROCR)
+library(gridExtra)
+
+data <- tbl_df(read.table("breast-cancer-wisconsin.data", strip.white = TRUE,
+                          sep=",", header = FALSE, na.strings = '?'))
+names(data) <- c('id', 'thickness', 'unif_cell_size', 'unif_cell_shape',
+                 'marginal_adhesion', 'cell_size', 'bare_nuclei',
+                 'bland_cromatin', 'normal_nucleoli', 'mitoses', 'class')
+
+glimpse(data)
+
+# 1. 결측치 처리
+data$bare_nuclei[is.na(data$bare_nuclei)] <- median(data$bare_nuclei, na.rm = TRUE)
+# 2. id 변수 제거
+data <- data %>% dplyr::select(-id)
+# 3. class 변수를 인자 변수로 변환
+data$class <- factor(ifelse(data$class == 2, 0, 1))
+
+glimpse(data)
+
+
+summary(data)
+
+pairs(data %>% sample_n(min(1000, nrow(data))))
+
+pairs(data %>% sample_n(min(1000, nrow(data))),
+      lower.panel=function(x,y){ points(x,y); abline(0, 1, col='red')},
+      upper.panel = panel.cor)
+
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
+p1 <- data %>% ggplot(aes(class)) + geom_bar()
+p2 <- data %>% ggplot(aes(class, unif_cell_size)) +
+  geom_jitter(col='gray') +
+  geom_boxplot(alpha=.5)
+p3 <- data %>% ggplot(aes(class, bare_nuclei)) +
+  geom_jitter(col='gray') +
+  geom_boxplot(alpha=.5)
+p4 <- data %>% ggplot(aes(unif_cell_size, bare_nuclei)) +
+  geom_jitter(col='gray') + geom_smooth()
+grid.arrange(p1, p2, p3, p4, ncol=2)
+
+
+# 트래인셋과 테스트셋의 구분
+set.seed(1606)
+n <- nrow(data)
+idx <- 1:n
+training_idx <- sample(idx, n * .60)
+idx <- setdiff(idx, training_idx)
+validate_idx <- sample(idx, n * .20)
+test_idx <- setdiff(idx, validate_idx)
+training <- data[training_idx,]
+validation <- data[validate_idx,]
+test <- data[test_idx,]
+
+
+# 선형회귀모형 (linear regression model)
+data_lm_full <- lm(quality ~ ., data=training)
+summary(data_lm_full)
+
+predict(data_lm_full, newdata = data[1:5,])
+
+# 선형회귀모형에서 변수선택
+data_lm_full_2 <- lm(quality ~ .^2, data=training)
+summary(data_lm_full_2)
+
+length(coef(data_lm_full_2))
+
+library(MASS)
+data_step <- stepAIC(data_lm_full,
+                     scope = list(upper = ~ .^2, lower = ~1))
+
+data_step
+anova(data_step)
+summary(data_step)
+length(coef(data_step))
+
+
+# 모형평가
+y_obs <- validation$quality
+yhat_lm <- predict(data_lm_full, newdata=validation)
+yhat_lm_2 <- predict(data_lm_full_2, newdata=validation)
+yhat_step <- predict(data_step, newdata=validation)
+rmse(y_obs, yhat_lm)
+rmse(y_obs, yhat_lm_2)
+rmse(y_obs, yhat_step)
+
+
+# 라쏘 모형 적합
+xx <- model.matrix(quality ~ .^2-1, data)
+# xx <- model.matrix(quality ~ .-1, data)
+x <- xx[training_idx, ]
+y <- training$quality
+glimpse(x)
+
+data_cvfit <- cv.glmnet(x, y)
+plot(data_cvfit)
+
+
+coef(data_cvfit, s = c("lambda.1se"))
+coef(data_cvfit, s = c("lambda.min"))
+
+
+predict.cv.glmnet(data_cvfit, s="lambda.min", newx = x[1:5,])
+
+y_obs <- validation$quality
+yhat_glmnet <- predict(data_cvfit, s="lambda.min", newx=xx[validate_idx,])
+yhat_glmnet <- yhat_glmnet[,1] # change to a vector from [n*1] matrix
+rmse(y_obs, yhat_glmnet)
+
+# 나무모형
+data_tr <- rpart(quality ~ ., data = training)
+data_tr
+
+printcp(data_tr)
+summary(data_tr)
+
+opar <- par(mfrow = c(1,1), xpd = NA)
+plot(data_tr)
+text(data_tr, use.n = TRUE)
+par(opar)
+
+
+yhat_tr <- predict(data_tr, validation)
+rmse(y_obs, yhat_tr)
+
+
+# 랜덤포레스트
+set.seed(1607)
+data_rf <- randomForest(quality ~ ., training)
+data_rf
+
+opar <- par(mfrow=c(1,2))
+plot(data_rf)
+varImpPlot(data_rf)
+par(opar)
+
+
+yhat_rf <- predict(data_rf, newdata=validation)
+rmse(y_obs, yhat_rf)
+
+
+# 부스팅
+set.seed(1607)
+data_gbm <- gbm(quality ~ ., data=training,
+                n.trees=40000, cv.folds=3, verbose = TRUE)
+(best_iter = gbm.perf(data_gbm, method="cv"))
+
+yhat_gbm <- predict(data_gbm, n.trees=best_iter, newdata=validation)
+rmse(y_obs, yhat_gbm)
+
+
+# 최종 모형선택과  테스트셋 오차계산
+data.frame(lm = rmse(y_obs, yhat_step),
+           glmnet = rmse(y_obs, yhat_glmnet),
+           rf = rmse(y_obs, yhat_rf),
+           gbm = rmse(y_obs, yhat_gbm)) %>%
+  reshape2::melt(value.name = 'rmse', variable.name = 'method')
+
+rmse(test$quality, predict(data_rf, newdata = test))
+
+
+# 회귀분석의 오차의 시각화
+boxplot(list(lm = y_obs-yhat_step,
+             glmnet = y_obs-yhat_glmnet,
+             rf = y_obs-yhat_rf,
+             gbm = y_obs-yhat_gbm), ylab="Error in Validation Set")
+abline(h=0, lty=2, col='blue')
+
+
+pairs(data.frame(y_obs=y_obs,
+                 yhat_lm=yhat_step,
+                 yhat_glmnet=c(yhat_glmnet),
+                 yhat_rf=yhat_rf,
+                 yhat_gbm=yhat_gbm),
+      lower.panel=function(x,y){ points(x,y); abline(0, 1, col='red')},
+      upper.panel = panel.cor)
+
